0f7533ab498d33105c87051b7967e49fba57904a
Jonathan Stegall <jstegall@minnpost.com>
2015-12-17 14:35:26 -0600

    forgot app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if app.config['ENABLE_SENTRY']:
 def minnpost_form():
     form = DonateForm()
     if request.args.get('amount'):
-        amount = request.args.get('amount')
+        amount = int(request.args.get('amount'))
     else:
         message = "The page you requested can't be found."
         return render_template('error.html', message=message)
@@ -67,11 +67,49 @@ def minnpost_form():
         yearly = 1
     installments = 'None'
     openended_status = 'Open'
+    level = checkLabel(amount, frequency, yearly)
     return render_template('minnpost-form.html', form=form, amount=amount,
         frequency=frequency, installments=installments,
         openended_status=openended_status,
         yearly=yearly,
+        level=level,
         key=app.config['STRIPE_KEYS']['publishable_key'])
+
+
+def checkLabel(amount, frequency, yearly, prior_year_amount=None, coming_year_amount=None, annual_recurring_amount=None):
+    thisyear = amount * yearly
+    level = ''
+    levelnum = ''
+    levelint = ''
+
+    if prior_year_amount != None or coming_year_amount != None or annual_recurring_amount != None:
+        if frequency is 'one-time':
+            prior_year_amount = thisyear
+        else:
+            annual_recurring_amount += thisyear
+
+        thisyear = max(prior_year_amount, coming_year_amount, annual_recurring_amount)
+
+    if (thisyear > 0 and thisyear < 60):
+        level = 'bronze'
+        levelnum = 'one'
+        levelint = 1
+    elif (thisyear > 59 and thisyear < 120):
+        level = 'silver'
+        levelnum = 'two'
+        levelint = 2
+    elif (thisyear > 119 and thisyear < 240):
+        level = 'gold'
+        levelnum = 'three'
+        levelint = 3
+    elif (thisyear > 239):
+        level = 'platinum'
+        levelnum = 'four'
+        levelint = 4
+
+    leveldata = {'level': level, 'levelnum': levelnum, 'levelint': levelint}
+    return leveldata
+
 
 
 @app.route('/memberform')
