bd6c73e0b31a2b7e95b9ed495d71636fd3c9a28a
Jonathan Stegall <jstegall@minnpost.com>
2015-12-23 10:50:50 -0600

    fix root issue. also move jQuery.

--- static/js/src/main.js
+++ static/js/src/main.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
 	// call plugin and pass options if need be
 	$('.support--forms').minnpost_giving({
-		'minnpost_root' : 'http://minnpost.dev',
 		'debug' : true
 	});
 });
