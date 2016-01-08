959a1f10aecc57d76a87fe1b379c2fd00c97d7ff
Jonathan Stegall <jstegall@minnpost.com>
2016-01-08 09:13:30 -0600

    fix charge object fields we are looking for

--- batch.py
+++ batch.py
@@ -94,9 +94,9 @@ def process_charges(query, log):
         update = {
                 'Stripe_Transaction_Id__c': charge.id,
                 'Stripe_Card__c': charge.source.id,
-                'Card_type__c': charge.brand,
-                'Card_expiration_date__c': charge.exp_month + ' / ' + charge.exp_year,
-                'Card_acct_last_4__c': charge.last4,
+                'Card_type__c': charge.source.brand,
+                'Card_expiration_date__c': charge.source.exp_month + ' / ' + charge.source.exp_year,
+                'Card_acct_last_4__c': charge.source.last4,
                 'StageName': 'Closed Won',
                 }
         path = item['attributes']['url']
