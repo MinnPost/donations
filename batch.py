0c8fe6ffe073996bd7d65104c3f22f8b724d4f7f
Jonathan Stegall <jstegall@minnpost.com>
2016-02-29 12:28:15 -0600

    another

--- batch.py
+++ batch.py
@@ -70,14 +70,9 @@ def process_charges(query, log):
 
             if item['Shipping_address_name__c'] != '':
                 shipping_address = {'line1' : item['Shipping_address_street__c'], 'city' : item['Shipping_address_city__c'], 'state' : item['Shipping_address_state__c'], 'postal_code' : item['Shipping_address_ZIP__c'], 'country' : item['Shipping_address_country__c']}
-                print('shipping')
-                print(shipping_address)
-                if shipping_address.setdefault('line1', '') != '':
-                    print('should not be empty here')
-                    print(shipping_address['line1'])
+                if shipping_address.get('line1'):
                     shipping_details = {'name' : item['Shipping_address_name__c'], 'address' : shipping_address}
                 else:
-                    print('no shipping. take that stufff out')
                     shipping_details = NULL
             else:
                 shipping_details = NULL
