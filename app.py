0695127787fca871748aba7f83d143878f255eec
Jonathan Stegall <jstegall@minnpost.com>
2016-12-02 13:45:18 -0600

    try to remove non numeric/decimal point characters from amount value

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ LOGGING = {
 def minnpost_form():
     form = MinnPostForm()
     if request.args.get('amount'):
-        amount = float(request.args.get('amount'))
+        amount = float(re.sub('[^0123456789\.]','',request.args.get('amount')))
         if (amount).is_integer():
             amount_formatted = int(amount)
         else:
