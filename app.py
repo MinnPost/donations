672a93e5b61d8e46f1a684708ad9a7191ccb9a8e
Jonathan Stegall <jstegall@minnpost.com>
2017-08-01 11:55:53 -0500

    typo

--- app.py
+++ app.py
@@ -1161,7 +1161,7 @@ def charge_ajax():
         if pay_fees == '1':
             # get fee amount to send to stripe; user does not see this
             if 'payment_type' in request.form:
-                session['payment_type'] = request.form.payment_type
+                session['payment_type'] = request.form['payment_type']
 
     email = request.form['email']
     first_name = request.form['first_name']
