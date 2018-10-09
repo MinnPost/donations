061c9e8f894cfc182a5a7fe0a85a85d8f14a876d
Jonathan Stegall <jstegall@minnpost.com>
2018-10-09 09:08:48 -0500

    let's start switching from rabbitmq to cloudamqp

--- app_celery.py
+++ app_celery.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 
 def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery = Celery(app.import_name, broker=app.config['CLOUDAMQP_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
 
