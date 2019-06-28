a8b41484a969c214ce21756a39bb6caa37e4b614
Jonathan Stegall <jstegall@minnpost.com>
2019-06-28 14:50:39 -0500

    remove the redundant pledge pay url

--- app.py
+++ app.py
@@ -632,6 +632,12 @@ def minnroast_patron_form():
         key=app.config['STRIPE_KEYS']['publishable_key']
     )
 
+# used at support.minnpost.com/minnroast-pledge
+@app.route('/minnroast-pledge/')
+def minnroast_pledge_form():
+    full_url = url_for('minnpost_pledge_payment', **request.args)
+    return redirect(full_url)
+
 
 # used at support.minnpost.com/pledge-payment
 @app.route('/pledge-payment/')
@@ -1127,122 +1133,6 @@ def minnpost_donation_cancel_form():
         first_name = first_name,last_name = last_name, email=email,
         minnpost_root = app.minnpost_root,
         key = app.config['STRIPE_KEYS']['publishable_key']
-    )
-
-
-# used at support.minnpost.com/minnroast-pledge
-@app.route('/minnroast-pledge/')
-def minnroast_pledge_form():
-    form = MinnPostForm()
-
-    confirm_url = '/minnroast-pledge-confirm/'
-    redirect_url = 'minnroast-pledge-thanks'
-
-    now = datetime.now()
-    year = now.year
-
-    if request.args.get('opportunity'):
-        opp_id = request.args.get('opportunity')
-        try:
-            result = get_opportunity(opp_id)
-            opportunity = result['opportunity']
-
-            amount = float(opportunity['Amount'])
-            if (amount).is_integer():
-                amount_formatted = int(amount)
-            else:
-                amount_formatted = format(amount, ',.2f')
-            campaign = opportunity['CampaignId']
-
-        except:
-            opp_id = ''
-            opportunity = []
-    else:
-        opp_id = ''
-        opportunity = []
-
-        if request.args.get('amount'):
-            amount = float(re.sub('[^\d\.]','',request.args.get('amount')))
-            if (amount).is_integer():
-                amount_formatted = int(amount)
-            else:
-                amount_formatted = format(amount, ',.2f')
-        else:
-            amount_formatted = ''
-
-        if request.args.get('campaign'):
-            campaign = request.args.get('campaign')
-        else:
-            campaign = ''
-
-    if request.args.get('show_ach'):
-        show_ach = request.args.get('show_ach')
-        if show_ach == 'true':
-            show_ach = True
-        else:
-            show_ach = False
-    else:
-        show_ach = SHOW_ACH
-
-    if request.args.get('customer_id'):
-        customer_id = request.args.get('customer_id')
-    elif 'Stripe_Customer_ID__c' in opportunity and opportunity['Stripe_Customer_ID__c'] is not None:
-        customer_id = opportunity['Stripe_Customer_ID__c']
-    else:
-        customer_id = ''
-
-    if request.args.get('pledge'):
-        pledge = request.args.get('pledge')
-    elif 'MRpledge_com_ID__c' in opportunity and opportunity['MRpledge_com_ID__c'] is not None:
-        pledge = opportunity['MRpledge_com_ID__c']
-    else:
-        pledge = ''
-
-    if request.args.get('firstname'):
-        first_name = request.args.get('firstname')
-    elif 'Donor_first_name__c' in opportunity and opportunity['Donor_first_name__c'] is not None:
-        first_name = opportunity['Donor_first_name__c']
-    else:
-        first_name = ''
-    if request.args.get('lastname'):
-        last_name = request.args.get('lastname')
-    elif 'Donor_last_name__c' in opportunity and opportunity['Donor_last_name__c'] is not None:
-        last_name = opportunity['Donor_last_name__c']
-    else:
-        last_name = ''
-    if request.args.get('email'):
-        email = request.args.get('email')
-    elif 'Donor_e_mail__c' in opportunity and opportunity['Donor_e_mail__c'] is not None:
-        email = opportunity['Donor_e_mail__c']
-    else:
-        email = ''
-    if request.args.get('additional_donation'):
-        additional_donation = float(request.args.get('additional_donation'))
-    else:
-        additional_donation = ''
-
-    title = 'MinnPost | MinnRoast Pledge'
-    if amount_formatted != '':
-        heading = '${} Donation for Election Coverage'.format(amount_formatted)
-    else:
-        heading = 'Donation for Election Coverage'
-    summary = 'Thank you for supporting our election coverage at MinnRoast. Please enter your information below to fulfill your pledge. If you have any questions, contact Tanner Curl at <a href="mailto:tcurl@minnpost.com">tcurl@minnpost.com</a>.'
-
-    button = 'Place this Donation'
-
-    description = 'MinnRoast Pledge'
-    allow_additional = False
-
-    return render_template(
-        'minnpost-minimal-form.html',
-        title=title, confirm_url=confirm_url, redirect_url=redirect_url, opp_id=opp_id, pledge=pledge, heading=heading,
-        description=description, summary=summary, allow_additional=allow_additional, button=button,
-        form=form, amount=amount_formatted, campaign=campaign, customer_id=customer_id,
-        #opp_type = opp_type, opp_subtype = opp_subtype,
-        first_name = first_name,last_name = last_name, email=email,
-        additional_donation = additional_donation,
-        show_ach = show_ach, plaid_env=PLAID_ENVIRONMENT, plaid_public_key=PLAID_PUBLIC_KEY, minnpost_root = app.minnpost_root,
-        key=app.config['STRIPE_KEYS']['publishable_key']
     )
 
 # used at support.minnpost.com/anniversary-sponsorship
