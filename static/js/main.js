2b641537a2375569cec717209c66f3b25d210c55
Jonathan Stegall <jstegall@minnpost.com>
2019-07-24 14:13:38 -0500

    quick fix for declining nyt even though it doesn't have the same name attribute anymore

--- static/js/main.js
+++ static/js/main.js
@@ -875,6 +875,8 @@ global.Payment = Payment;
     'upsell_selector' : '.well--upsell',
     'upsell_amount_selector' : '.upsell-amount',
     'swag_selector' : '.swag',
+    'swag_decline_selector' : '#swag-no',
+    'swag_nyt_selector' : 'input[name="nyt"]',
     'separate_swag_selector' : 'fieldset.swag--separate',
     'separate_swag_redeem' : '.swag-redeem--separate',
     'swag_selector_choose_multiple' : '.swag--choose-multiple',
@@ -1481,6 +1483,10 @@ global.Payment = Payment;
 
       $(options.atlantic_status, element).change(function() { // if user clicks one of the atlantic radio buttons
         that.swag(element, options, true);
+      });
+
+      $(options.swag_decline_selector, element).click(function(event) {
+        $(options.swag_nyt_selector, element).prop('checked', false);
       });
 
       var maximum_choose = $(options.swag_selector_choose_multiple, element).data('maximum-choose');
