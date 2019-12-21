910d63fe683e5d9e430adb3f56cf2eb3ec8fe90b
Jonathan Stegall <jstegall@minnpost.com>
2019-12-21 16:11:59 -0600

    stripe ach pending, also error handling

--- charges.py
+++ charges.py
@@ -116,71 +116,92 @@ def charge(opportunity):
     logging.info(
         f"---- Charging ${amount} to {opportunity.stripe_customer_id} ({opportunity.name})"
     )
-    if opportunity.stage_name != "Pledged":
-        raise Exception(f"Opportunity {opportunity.id} is not Pledged")
 
-    opportunity.stage_name = "In Process"
-    opportunity.save()
+    if opportunity.stage_name == 'Pledged':
+        opportunity.stage_name = "In Process"
+        opportunity.save()
 
-    if opportunity.stripe_card is not None:
-        charge_source = opportunity.stripe_card
-    elif opportunity.stripe_bank_account is not None:
-        charge_source = opportunity.stripe_bank_account
-    else:
-        charge_source = None
+        if opportunity.stripe_card is not None:
+            charge_source = opportunity.stripe_card
+        elif opportunity.stripe_bank_account is not None:
+            charge_source = opportunity.stripe_bank_account
+        else:
+            charge_source = None
 
-    if opportunity.shipping_name != '':
-        shipping_address = {'line1' : opportunity.shipping_street, 'city' : opportunity.shipping_city, 'state' : opportunity.shipping_state, 'postal_code' : opportunity.shipping_zip, 'country' : opportunity.shipping_country}
-        if shipping_address.get('line1'):
-            shipping_details = {'name' : opportunity.shipping_name, 'address' : shipping_address}
+        if opportunity.shipping_name != '':
+            shipping_address = {'line1' : opportunity.shipping_street, 'city' : opportunity.shipping_city, 'state' : opportunity.shipping_state, 'postal_code' : opportunity.shipping_zip, 'country' : opportunity.shipping_country}
+            if shipping_address.get('line1'):
+                shipping_details = {'name' : opportunity.shipping_name, 'address' : shipping_address}
+            else:
+                shipping_details = None
         else:
             shipping_details = None
+
+        try:
+            card_charge = stripe.Charge.create(
+                customer=opportunity.stripe_customer_id,
+                amount=int(amount * 100),
+                currency="usd",
+                description=opportunity.stripe_description,
+                metadata={
+                    "opportunity_id": opportunity.id,
+                    "account_id": opportunity.account_id,
+                    "source": opportunity.referring_page,
+                },
+                shipping=shipping_details,
+                source=charge_source,
+            )
+        except stripe.error.CardError as e:
+            # look for decline code:
+            error = e.json_body["error"]
+            logging.info(f"The card has been declined:")
+            logging.info(f"Message: {error.get('message', '')}")
+            logging.info(f"Decline code: {error.get('decline_code', '')}")
+            opportunity.stripe_error_message = error.get("message", "unknown failure")
+            opportunity.stage_name = "Failed"
+            opportunity.save()
+            logging.debug(
+                f"Opportunity set to '{opportunity.stage_name}' with reason: {opportunity.stripe_error_message}"
+            )
+            return
+
+        except stripe.error.InvalidRequestError as e:
+            logging.error(f"Problem: {e}")
+            # TODO should we raise this?
+            return
+        except Exception as e:
+            logging.error(f"Problem: {e}")
+            # TODO should we raise this?
+            return
+
+        if card_charge.status != "succeeded":
+            logging.error("Charge failed. Check Stripe logs.")
+            opportunity.stage_name = "Failed"
+            opportunity.stripe_error_message = "Error: Unknown. Check logs"
+            opportunity.save()
+            return
+
     else:
-        shipping_details = None
-
-    try:
-        card_charge = stripe.Charge.create(
-            customer=opportunity.stripe_customer_id,
-            amount=int(amount * 100),
-            currency="usd",
-            description=opportunity.stripe_description,
-            metadata={
-                "opportunity_id": opportunity.id,
-                "account_id": opportunity.account_id,
-                "source": opportunity.referring_page,
-            },
-            shipping=shipping_details,
-            source=charge_source,
+        logging.info(
+            f"---- Checking transaction {opportunity.stripe_transaction_id}"
         )
-    except stripe.error.CardError as e:
-        # look for decline code:
-        error = e.json_body["error"]
-        logging.info(f"The card has been declined:")
-        logging.info(f"Message: {error.get('message', '')}")
-        logging.info(f"Decline code: {error.get('decline_code', '')}")
-        opportunity.stripe_error_message = error.get("message", "unknown failure")
-        opportunity.stage_name = "Failed"
+        charge = stripe.Charge.retrieve(opportunity.stripe_transaction_id)
+
+    # this is either pending or finished
+    if charge.status == 'pending':
+            logging.info(f"ACH charge pending. Check at intervals to see if it processes.")
+            opportunity.stage_name = "ACH Pending"
+            opportunity.stripe_transaction_id = charge.id
+            opportunity.save()
+
+    # charge was successful
+    if charge.source.object != 'bank_account':
+        opportunity.stripe_card = card_charge.source.id
+        opportunity.stripe_transaction_id = card_charge.id
+        opportunity.stage_name = "Closed Won"
         opportunity.save()
-        logging.debug(
-            f"Opportunity set to '{opportunity.stage_name}' with reason: {opportunity.stripe_error_message}"
-        )
-        return
-
-    except stripe.error.InvalidRequestError as e:
-        logging.error(f"Problem: {e}")
-        # TODO should we raise this?
-        return
-    except Exception as e:
-        logging.error(f"Problem: {e}")
-        # TODO should we raise this?
-        return
-
-    if card_charge.status != "succeeded":
-        logging.error("Charge failed. Check Stripe logs.")
-        # TODO should we raise this?
-        return
-
-    opportunity.stripe_card = card_charge.source.id
-    opportunity.stripe_transaction_id = card_charge.id
-    opportunity.stage_name = "Closed Won"
-    opportunity.save()
+    else:
+        opportunity.stripe_card = card_charge.source.id
+        opportunity.stripe_bank_account = charge.source.id
+        opportunity.stage_name = "Closed Won"
+        opportunity.save()
