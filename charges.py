478233eccdd3179d0dd828e805f631aa2f241b9d
Jonathan Stegall <jstegall@minnpost.com>
2020-10-28 12:58:47 -0500

    start #117

--- charges.py
+++ charges.py
@@ -288,33 +288,31 @@ def charge(opportunity):
             payment_intent = stripe.PaymentIntent.retrieve(
                 opportunity.stripe_transaction_id,
             )
+            charge_id = payment_intent.charges.data[0].id
+            charge = stripe.Charge.retrieve(charge_id)
         elif charge_source is not None:
             charge = stripe.Charge.retrieve(opportunity.stripe_transaction_id)
 
-    if (payment_intent and payment_intent.status != 'succeeded') or (charge and charge.status != "succeeded" and charge.status != "pending"):
-        logging.error("Charge failed. Check Stripe logs.")
-        raise ChargeException(opportunity, "charge failed")
-    
-    # this is either pending or finished
-    if charge and charge.status == "pending":
-        logging.info(f"ACH charge pending. Check at intervals to see if it processes.")
-        opportunity.stage_name = "ACH Pending"
-        opportunity.stripe_transaction_id = charge.id
-        opportunity.save()
+    if charge:
 
-    # payment was successful
-    if payment_intent:
-        opportunity.stripe_card = payment_intent.payment_method
-        opportunity.stripe_transaction_id = payment_intent.id
-        opportunity.stage_name = "Closed Won"
-        opportunity.save()
-    elif charge and charge.source.object == "bank_account":
-        opportunity.stripe_bank_account = charge.source.id
-        opportunity.stage_name = "Closed Won"
-        opportunity.save()
-    elif charge:
-        opportunity.stripe_card = charge.source.id
-        opportunity.stripe_transaction_id = charge.id
-        opportunity.stage_name = "Closed Won"
-        opportunity.save()
+        if charge.status != "succeeded" and charge.status != "pending":
+            logging.error("Charge failed. Check Stripe logs.")
+            raise ChargeException(opportunity, "charge failed")
     
+        # this is either pending or finished
+        if charge.status == "pending":
+            logging.info(f"ACH charge pending. Check at intervals to see if it processes.")
+            opportunity.stage_name = "ACH Pending"
+            opportunity.stripe_transaction_id = charge.id
+            opportunity.save()
+
+        # payment was successful
+        if charge.source.object == "bank_account":
+            opportunity.stripe_bank_account = charge.source.id
+            opportunity.stage_name = "Closed Won"
+            opportunity.save()
+        else:
+            opportunity.stripe_card = charge.source.id
+            opportunity.stripe_transaction_id = charge.id
+            opportunity.stage_name = "Closed Won"
+            opportunity.save()
